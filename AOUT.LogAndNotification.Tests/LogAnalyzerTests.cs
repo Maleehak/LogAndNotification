e5ebe0e34e9bd9d8790dba1ade11eb0ab55e4702
maleehak <maleehakhalid604@gmail.com>
2021-09-16 17:22:34 +0500

    add state based unit testing

---
+++ AOUT.LogAndNotification.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LogAndNotification;
+
+namespace AOUT.LogAndNotification.Tests
+{
+    [TestFixture]
+    class LogAnalyzerTests
+    {
+        [Test]
+         public void IsValidLogFileName_ValidName_RemembersTrue()
+        {
+            LogAnalyzer log = new LogAnalyzer();
+            log.IsValidLogFileName("somefile.slf");
+            Assert.IsTrue(log.WasLastFileNameValid);
+        }
+    }
+}
