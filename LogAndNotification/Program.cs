f74d09530a5344b73e9d21329d93f6eabef35b06
maleehak <maleehakhalid604@gmail.com>
2021-09-16 17:00:54 +0500

    Add project files.

---
+++ LogAndNotification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogAndNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
